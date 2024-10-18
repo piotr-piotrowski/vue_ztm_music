f157dead53626d40725dcf7bc2391e9b9c08b973
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-10-18 17:14:28 +0200

    113. Validating Password

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -12,6 +12,7 @@ import {
   email,
   min_value as minValue,
   max_value as maxValue,
+  confirmed,
 } from "@vee-validate/rules"
 
 export default {
@@ -27,5 +28,6 @@ export default {
     defineRule("email", email)
     defineRule("min_value", minValue)
     defineRule("max_value", maxValue)
+    defineRule("confirmed", confirmed)
   },
 }
