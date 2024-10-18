b3d1e7e0daedb529a868bf88e12095fba77a3372
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-10-18 17:24:15 +0200

    115. Validating the Form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -90,7 +90,11 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -210,6 +214,11 @@ export default {
     ...mapWritableState(useModalStore, {
       modalVisibility: "isOpen",
     }),
+  },
+  methods: {
+    register(values) {
+      console.log(values)
+    },
   },
 }
 </script>
