bff4c8822b362d4741ebf826a5bd1cfd2be6c3ff
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-10-21 16:55:05 +0200

    138. Setting up the Login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,40 +1,28 @@
 <template>
   <!-- Login Form -->
-  <div
-    class="text-white text-center font-bold p-4 rounded mb-4"
-    v-if="login_show_alert"
-    :class="login_alert_variant"
-  >
+  <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="login_show_alert" :class="login_alert_variant">
     {{ login_alert_msg }}
   </div>
   <vee-form :validation-schema="loginSchema" @submit="login">
     <!-- Email -->
     <div class="mb-3">
       <label class="inline-block mb-2">Email</label>
-      <vee-field
-        type="email"
-        name="email"
+      <vee-field type="email" name="email"
         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Enter Email"
-      />
+        placeholder="Enter Email" />
       <ErrorMessage class="text-red-600" name="email" />
     </div>
     <!-- Password -->
     <div class="mb-3">
       <label class="inline-block mb-2">Password</label>
-      <vee-field
-        type="password"
-        name="password"
+      <vee-field type="password" name="password"
         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Password"
-      />
+        placeholder="Password" />
       <ErrorMessage class="text-red-600" name="password" />
     </div>
-    <button
-      type="submit"
+    <button type="submit"
       class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-      :disabled="login_in_submission"
-    >
+      :disabled="login_in_submission">
       Submit
     </button>
   </vee-form>
@@ -42,9 +30,14 @@
 
 <script>
 import { ErrorMessage } from "vee-validate"
+import { mapActions } from "pinia";
+import useUserStore from '@/stores/user'
 
 export default {
   name: "LoginForm",
+  components: {
+    ErrorMessage,
+  },
   data() {
     return {
       loginSchema: {
@@ -58,11 +51,24 @@ export default {
     }
   },
   methods: {
-    login(values) {
+    ...mapActions(useUserStore, ["authenticate"]),
+    async login(values) {
       this.login_show_alert = true
       this.login_in_submission = true
       this.login_alert_variant = "bg-blue-500"
       this.login_alert_msg = "Please wait! We are logging in."
+
+
+      try {
+        await this.authenticate(values)
+      } catch (error) {
+        console.log(error);
+        this.login_in_submission = false
+        this.login_alert_variant = "bg-red-500"
+        this.login_alert_msg =
+          "Invalid login details."
+        return
+      }
 
       this.login_alert_variant = "bg-green-500"
       this.login_alert_msg = "Success! You are now logged in."
--- src/stores/user.js
+++ src/stores/user.js
@@ -25,6 +25,11 @@ export default defineStore("user", {
       })
 
       this.userLoggedIn = true
-    }
-  }
+    },
+    async authenticate(values) {
+      await auth.signInWithEmailAndPassword(values.email, values.password)
+
+      this.userLoggedIn = true
+    },
+  },
 })
