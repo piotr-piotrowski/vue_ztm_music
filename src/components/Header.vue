d251f1566f69f198c1231f6f9987b2ec7efa5bea
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-10-24 18:41:43 +0200

    154. Redirecting after Logging Out

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,24 +3,37 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name: 'home'}" exact-active-class="no-active">Music</router-link>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <router-link class="px-2 text-white" :to="{name: 'about'}">About</router-link>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link
+            >
           </li>
           <li v-if="!userStore.userLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" :to="{name: 'manage'}">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                >Manage</router-link
+              >
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut"
+                >Logout</a
+              >
             </li>
           </template>
         </ul>
@@ -43,6 +56,12 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
       console.log(this.modalStore.isOpen)
+    },
+    signOut() {
+      this.userStore.signOut()
+      if (this.$route.name === "manage") {
+        this.$router.push({ name: "home" })
+      }
     },
   },
 }
