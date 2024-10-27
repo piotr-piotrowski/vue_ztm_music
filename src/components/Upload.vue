d1c25962b3522177362dbb62830f5c4c9854c6e4
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-10-27 19:48:34 +0100

    162. Adding the Progress Bar

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -21,32 +21,15 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm">{{ upload.name }}</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
             class="transition-all progress-bar bg-blue-400"
-            style="width: 75%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 35%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 55%"
+            :class="'bg-blue-400'"
+            :style="{ width: upload.current_progress + '%' }"
           ></div>
         </div>
       </div>
@@ -62,6 +45,7 @@ export default {
   data() {
     return {
       is_dragover: false,
+      uploads: [],
     }
   },
   methods: {
@@ -70,15 +54,26 @@ export default {
 
       const files = [...$event.dataTransfer.files]
 
-      files.forEach((file) => {
+      files.forEach(file => {
         if (file.type !== "audio/mpeg") {
-          console.log('different file type than audio/mpeg: ', file.type);
+          console.log("different file type than audio/mpeg: ", file.type)
           return
         }
 
         const storageRef = storage.ref()
         const songsRef = storageRef.child(`songs/${file.name}`)
-        songsRef.put(file)
+        const task = songsRef.put(file)
+
+        this.uploads.push({
+          task,
+          current_progress: 0,
+          name: file.name,
+        })
+
+        task.on("state_changed", snapshot => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        })
       })
     },
   },
