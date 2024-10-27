d107535d10e7dea8de14bed16c5c5f790e0ad265
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-10-27 20:58:42 +0100

    164. Improving the Progress Bar

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -23,12 +23,14 @@
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">{{ upload.name }}</div>
+        <div class="font-bold text-sm" :class="upload.text_class">
+          <i :class="upload.icon"></i>{{ upload.name }}
+        </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
             class="transition-all progress-bar bg-blue-400"
-            :class="'bg-blue-400'"
+            :class="upload.class"
             :style="{ width: upload.current_progress + '%' }"
           ></div>
         </div>
@@ -69,6 +71,9 @@ export default {
             task,
             current_progress: 0,
             name: file.name,
+            variant: "bg-blue-400",
+            icon: "fas fa-spinner fa-spin",
+            text_class: "",
           }) - 1
 
         task.on("state_changed", snapshot => {
