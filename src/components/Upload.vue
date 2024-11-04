fbbdc8721b25d6a343d1a56df1c8f9fa5a09cea8
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-04 17:06:07 +0100

    170. Cancelling Uploads with Refs

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -112,6 +112,11 @@ export default {
         )
       })
     },
+    // cancelUploads() {
+    //   this.uploads.forEach(upload => {
+    //     upload.task.cancel()
+    //   })
+    // },
   },
   beforeUnmount() {
     this.uploads.forEach(upload => {
--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -3,7 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <app-upload />
+        <app-upload ref="upload" />
       </div>
       <div class="col-span-2">
         <div
@@ -140,6 +140,11 @@ export default {
   components: {
     AppUpload,
   },
+  // beforeRouteLeave(to, from, next) {
+  //   console.log('beforeRouteLeave for Mange.vue')
+  //   this.$refs.upload.cancelUploads()
+  //   next()
+  // }
   // beforeRouteEnter(to, from, next) {
   //   const store = useUserStore()
 
