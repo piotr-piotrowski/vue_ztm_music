0bffed74d5773f65ca62e5f98ecaf2717c23a4ba
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-04 18:10:00 +0100

    177. Validating the Song Form

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -15,22 +15,30 @@
       </button>
     </div>
     <div v-show="showForm">
-      <form>
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input
+          <vee-field
+            name="modified_name"
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input
+          <vee-field
+            name="genre"
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button
           type="submit"
@@ -44,14 +52,17 @@
         >
           Go Back
         </button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
 
 <script>
+import { ErrorMessage } from "vee-validate"
+
 export default {
   name: "CompositionItem",
+  components: { ErrorMessage },
   props: {
     song: {
       type: Object,
@@ -61,7 +72,16 @@ export default {
   data() {
     return {
       showForm: false,
+      schema: {
+        modified_name: "required",
+        genre: "alpha_spaces",
+      },
     }
+  },
+  methods: {
+    edit() {
+      console.log("click submit")
+    },
   },
 }
 </script>
