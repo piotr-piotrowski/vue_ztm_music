0865ebf93327974811f6d4b66f1866846b74a52b
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-20 19:57:06 +0100

    191. Updating the Comment List

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -59,6 +59,7 @@
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          v-model="sort"
         >
           <option value="1">Latest</option>
           <option value="2">Oldest</option>
@@ -70,7 +71,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -106,10 +107,19 @@ export default {
       comment_alert_variant: "bg-blue-500",
       comment_alert_message: "Please wait! Your comment is being submitted.",
       comments: [],
+      sort: "1",
     }
   },
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort == "1") {
+          return new Date(b.datePosted) - new Date(a.datePosted)
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted)
+      })
+    },
   },
   async created() {
     const docSnapshot = await songCollection.doc(this.$route.params.id).get()
@@ -145,8 +155,6 @@ export default {
     },
 
     async getComments() {
-      console.log(this.$route.params.id);
-      
       const snapshot = await commentsCollection
         .where("sid", "==", this.$route.params.id)
         .get()
