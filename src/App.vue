24680fc25fe5e1d90fa4bd5957b9aead669bda50
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-21 17:48:55 +0100

    197. Toggling Audio

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,7 @@
 
   <router-view></router-view>
 
-  <!-- Player -->
-  <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
-    <!-- Track Info -->
-    <div class="text-center">
-      <span class="song-title font-bold">Song Title</span> by
-      <span class="song-artist">Artist</span>
-    </div>
-    <div class="flex flex-nowrap gap-4 items-center">
-      <!-- Play/Pause Button -->
-      <button type="button">
-        <i class="fa fa-play text-gray-500 text-xl"></i>
-      </button>
-      <!-- Current Position -->
-      <div class="player-currenttime">00:00</div>
-      <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
-        <!-- Player Ball -->
-        <span
-          class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
-          style="left: 50%"
-        >
-          <i class="fas fa-circle"></i>
-        </span>
-        <!-- Player Progress Bar-->
-        <span
-          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          style="width: 50%"
-        ></span>
-      </div>
-      <!-- Duration -->
-      <div class="player-duration">03:06</div>
-    </div>
-  </div>
+  <app-player />
 
   <auth />
 </template>
@@ -43,13 +11,14 @@
 <script>
 import { auth } from "@/includes/firebase"
 import AppHeader from "@/components/Header.vue"
+import AppPlayer from "@/components/Player.vue"
 import { mapWritableState } from "pinia"
 import useUserStore from "@/stores/user"
 import Auth from "@/components/Auth.vue"
 
 export default {
   name: "App",
-  components: { AppHeader, Auth },
+  components: { AppHeader, Auth, AppPlayer },
   data() {
     return {
       publicPath: import.meta.env.BASE_URL,
