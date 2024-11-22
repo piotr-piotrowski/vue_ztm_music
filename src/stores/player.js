b0bb830f8ce1c1148860d98974e1773c3f9b6e17
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-22 17:30:13 +0100

    200. Player Progress Bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -8,9 +8,14 @@ export default defineStore("player", {
     sound: {},
     seek: "00:00",
     duration: "00:00",
+    progressPlayer: "0%",
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload()
+      }
+
       this.currentSong = song
 
       this.sound = new Howl({
@@ -38,6 +43,8 @@ export default defineStore("player", {
     progress() {
       this.seek = helper.formatTime(this.sound.seek())
       this.duration = helper.formatTime(this.sound.duration())
+
+      this.progressPlayer = `${(this.sound.seek() / this.sound.duration()) * 100}%`
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress)
