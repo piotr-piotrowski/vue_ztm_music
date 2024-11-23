112b7318cd007ff70c15fec5d2e9b60e7786677d
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-23 18:06:37 +0100

    209. Registering a Directive Locally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,7 +33,7 @@
       >
         <div
           class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
-          v-icon.right="'headphones-alt'"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
         >
           <span class="card-title">Songs</span>
           <!-- Icon -->
@@ -51,6 +51,7 @@
 <script>
 import AppSongItem from "@/components/SongItem.vue"
 import { songCollection } from "@/includes/firebase"
+import IconSecondary from "@/directives/icon-secondary"
 
 export default {
   name: "Home",
@@ -64,6 +65,9 @@ export default {
   },
   components: {
     AppSongItem,
+  },
+  directives: {
+    "icon-secondary": IconSecondary,
   },
   async created() {
     this.getSongs()
