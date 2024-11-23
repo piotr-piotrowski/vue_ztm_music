c8eb1d97d904224d7240c8b99b1dbb9bee82e29a
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-23 14:49:44 +0100

    204. Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <app-header />
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <Component :is="Component"></Component>
+    </transition>
+  </router-view>
 
   <app-player />
 
@@ -34,3 +38,18 @@ export default {
   },
 }
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
