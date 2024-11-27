41d0c3d5a46997cc272972e31c1a43dd59bda9dd
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-27 10:39:36 +0100

    211. Our First Translation

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from "vue-i18n"
 import en from "@/locales/en"
+import pl from "@/locales/pl"
 
 export default createI18n({
   locale: "en",
   fallbackLocale: "en",
   messages: {
     en,
+    pl,
   },
 })
