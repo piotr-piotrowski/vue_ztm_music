9aebcf73059d4c810a105b15a5d562d6a7d3f3da
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-29 15:31:01 +0100

    218. The Manifest File - install vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
