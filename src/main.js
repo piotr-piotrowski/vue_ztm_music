d710f3f11dde018bddec61dbb2990b9558651d1f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-29 16:04:11 +0100

    221. Offline Support with Service Workers

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import router from "./router"
 import Icon from "@/directives/icon"
 
 import i18n from "./includes/i18n"
+import { registerSW } from "virtual:pwa-register"
+
+registerSW({ immediate: true })
 
 import "./assets/base.css"
 import "./assets/main.css"
