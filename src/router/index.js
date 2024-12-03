a10017906684d3807b079d3801a407b972480d2f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 16:03:36 +0100

    229. Dynamic Route Imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import About from "@/views/About.vue"
-import Home from "@/views/Home.vue"
-import Manage from "@/views/Manage.vue"
 import { createRouter, createWebHistory } from "vue-router"
 import useUserStore from "@/stores/user"
-import Song from "@/views/Song.vue"
+
+const Home = () => import("@/views/Home.vue")
+const Manage = () => import("@/views/Manage.vue")
+const About = () => import("@/views/About.vue")
+const Song = () => import("@/views/Song.vue")
 
 const routes = [
   {
